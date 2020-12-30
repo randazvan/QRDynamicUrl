6989a643fe3fb6e93efae38acee20b6880ed2d88
randazvan <d_a_razvan@yahoo.com>
2020-12-30 11:33:17 +0000

    Added VCARD and code refactored

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, send_file
-import pyqrcode as pq
+from flask import Flask, render_template
 import secondary_functions as sf
 
 app = Flask(__name__)
@@ -7,33 +6,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("index.html")
 
 
-@app.route('/api/qrcode/<text>')
+@app.route('/api/qrcode/text/<text>')
 def text_qr_code(text):
 
-    filename = sf.create_static_filepath()
-    qrcode = pq.create(text)
-    qrcode.png(filename, scale=10, module_color=sf.default_colour, background=sf.default_background)
-
-    return send_file(filename)\
+    return sf.create_text_qr_code(text)
 
 
-@app.route('/api/qrcode/<text>/<code_colour>/<background_colour>')
-def text_colours_qr_code(text, code_colour, background_colour):
+@app.route('/api/qrcode/text/<text>/<background_colour>')
+def text_colours_qr_code(text, background_colour):
 
-    #converting colours from hex to RGB
-    hex_code_colour = sf.hex_to_rgb(code_colour)
-    hex_background_colour = sf.hex_to_rgb(background_colour)
-    filename = sf.create_static_filepath()
-    qrcode = pq.create(text)
-    qrcode.png(filename,
-               scale=10,
-               module_color=hex_code_colour if hex_code_colour else sf.default_colour,
-               background=hex_background_colour if hex_background_colour else sf.default_background)
+    return sf.create_text_qr_code(text, background_colour)
 
-    return send_file(filename)
+
+@app.route('/api/qrcode/vcard/<text>')
+def vcard_qr_code(text):
+
+    return sf.create_text_qr_code(text, vcard=1)
+
+
+@app.route('/api/qrcode/vcard/<text>/<background_colour>')
+def vcard_colours_qr_code(text, background_colour):
+
+    return sf.create_text_qr_code(text, background_colour, vcard=1)
 
 
 if __name__ == '__main__':
--- secondary_functions.py
+++ secondary_functions.py
@@ -1,23 +1,36 @@
 from datetime import datetime
+import pyqrcode as pq
+from flask import send_file
+import os
+import ast
 
-default_colour = [0, 0, 0, 128]
-default_background = [255, 255, 255]
 
+# Default variables of the QR code
+default_colour = '000000'
+default_background = 'ffffff'
+default_qr_version = 6
+default_qr_error = 'M'
+default_png_scale = 10
+
+
+# Get the current time in milliseconds
 def get_current_dt_in_millisec():
-    dt_obj = datetime.strptime('20.12.2016 09:38:42,76',
-                               '%d.%m.%Y %H:%M:%S,%f')
-    millisec = dt_obj.timestamp()
+    dt_obj = datetime.now()
+
+    millisec = int(dt_obj.timestamp())
 
     return millisec
 
 
+# Creating the static file to save the QR code
 def create_static_filepath(file_type='png'):
-    return "qrcodes/" + str(get_current_dt_in_millisec()) + "." + file_type
+    return os.path.join(os.path.dirname(__file__), 'qrcodes/') + str(get_current_dt_in_millisec()) + "." + file_type
 
 
+# Transform HEX colour from URL in RGB with transparency variable
 def hex_to_rgb(hex_colour, transparency=255):
-    hex_colour = remove_string_char(hex_colour)
-    if(len(hex_colour) == 6):
+    hex_colour = hex_colour.replace("'", "")
+    if len(hex_colour) == 6:
         rgb = [int(hex_colour[i:i+2], 16) for i in (0, 2, 4)]
         rgb.append(transparency)
         return rgb
@@ -25,14 +38,58 @@ def hex_to_rgb(hex_colour, transparency=255):
         return None
 
 
+# Transform HEX colour from URL in RGB without transparency variable
 def hex_to_rgb_no_transparency(hex_colour):
-    hex_colour = remove_string_char(hex_colour)
-    if (len(hex_colour) == 6):
+    hex_colour = hex_colour.replace("'", "")
+    if len(hex_colour) == 6:
         rgb = [int(hex_colour[i:i+2], 16) for i in (0, 2, 4)]
         return rgb
     else:
         return None
 
-def remove_string_char(string):
-    return string.replace("'", "")
+
+# creating a text qr code
+def create_text_qr_code(text,
+                        background_colour=default_background,
+                        qr_error=default_qr_error,
+                        qr_version=default_qr_version,
+                        vcard=0):
+
+    # check if we need to create VCARD data
+    if vcard == 1:
+        text = vcard_data_preparation(text)
+        qr_version = 6
+
+    # image destination file
+    filename = create_static_filepath()
+
+    # Change HEX to RGB colour
+    hex_background_colour = hex_to_rgb(background_colour)
+
+    # create the QR code
+    qrcode = pq.create(text, error=qr_error, version=qr_version)
+    qrcode.png(filename, scale=default_png_scale, module_color=default_colour, background=hex_background_colour)
+
+    return send_file(filename)
+
+
+# Checking the VCARD data
+def vcard_data_preparation(text):
+
+    text.replace("'", '"')
+
+    vcard = {}
+    vcard['BEGIN'] = 'VCARD'
+    vcard.update(ast.literal_eval(text))
+    vcard['END'] = 'VCARD'
+
+    print(vcard)
+    vcard_text = ''
+
+    for key, value in vcard.items():
+        vcard_text += key + ':' + value + '\n'
+
+    print(vcard_text)
+
+    return vcard_text
 
